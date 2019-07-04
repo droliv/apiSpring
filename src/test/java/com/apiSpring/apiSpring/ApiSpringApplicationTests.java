792aee8b4607ec258eacea8c2f3ff070a83023f9
Daniele Reis <reis-daniele@hotmail.com>
2019-07-04 11:30:46 -0300

    Primeiro Commit

---
+++ src/test/java/com/apiSpring/apiSpring/ApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiSpring.apiSpring;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
